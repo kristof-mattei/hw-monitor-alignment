006a1b66412e4f7764ad75a06f80d1c9267d149e
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-07-01 15:18:17 -0700

    fix: add winmd files for building, and then format generated file

--- crates/hw-monitor-alignment/src/build.rs
+++ crates/hw-monitor-alignment/src/build.rs
@@ -1,3 +1,34 @@
 fn main() {
     windows_reactor_setup::as_framework_dependent();
+
+    let output_path = "src/bindings.rs";
+
+    let args = [
+        "--in",
+        "../../winmd",
+        "--out",
+        output_path,
+        "--implement",
+        "Microsoft.UI.Xaml.IApplicationOverrides",
+        "Microsoft.UI.Xaml.Markup.IXamlMetadataProvider",
+        "--minimal",
+        "--flat",
+        "--filter",
+        "Microsoft.UI.Xaml.ISizeChangedEventArgs::{get_NewSize}",
+        "Microsoft.UI.Xaml.IFrameworkElement::{add_SizeChanged}",
+    ];
+
+    windows_bindgen::bindgen(args);
+
+    let status = Command::new("rustfmt")
+        .arg(output_path)
+        .status()
+        .expect("Failed to execute rustfmt process");
+
+    if !status.success() {
+        println!(
+            "cargo:warning=Failed to format {}: rustfmt exited with {:?}",output_path
+            status.code()
+        );
+    }
 }
